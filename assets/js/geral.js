5c3c8ab12c7834186da0d6a16f41d10b011b3784
José Renato dos Santos Pereira <jrpereira@senacrs.com.br>
2022-05-12 17:57:32 -0300

    Adicionado conteudo até pagina 2 bloco 7, parou no bloco 8

---
+++ assets/js/geral.js
@@ -0,0 +1,18 @@
+window.addEventListener('load', function(){ 
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+      return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+
+    $('.preloader').fadeOut(300);
+})
+
+$('.tabela-interativa').on('mouseover', function(){
+
+  $(this).removeClass('fechada')
+})
+
+$('.tabela-interativa').on('mouseout', function(){
+
+  $(this).addClass('fechada')
+})
